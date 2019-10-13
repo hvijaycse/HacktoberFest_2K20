21b1fe9a47d627db15efbcc0e3db1ed90e391908
Aman Raj <41481755+Aman199825@users.noreply.github.com>
2019-10-13 22:55:53 +0530

    Update UniqueBinarySearchTrees.java

--- Intermediate/Data Structures/UniqueBinarySearchTrees.java
+++ Intermediate/Data Structures/UniqueBinarySearchTrees.java
@@ -1,14 +1,17 @@
-class UniqueBinarySearchTrees{
+class Solution {
     public int numTrees(int n)
     {
-      int [] dp=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]+=dp[i-1]*dp[n-i];
-        }
-        System.out.println(dp[2]);
+          int [] dp=new int[n+1];
+      dp[0]=1;
+      dp[1]=1;
+      for(int i=2;i<=n;i++)
+      {
+    	  dp[i]=0;
+    	  for(int j=0;j<i;j++)
+    	  {
+    		  dp[i]+=dp[j]*dp[i-j-1];
+    	  }
+      }
         return dp[n];
     }
 }
