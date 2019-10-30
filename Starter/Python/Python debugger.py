554c5d30dbab5a6850afdac516924ef3529d7aa7
Rajani169 <sharmarajani169@gmail.com>
2019-10-30 12:40:03 +0530

    This is it

---
+++ Starter/Python/Python debugger.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+x = [1,2,3]
+y=3
+z=4
+
+result = y+z
+print(result)
+result1 = x+y
+print(result1)
+
+
+# In[6]:
+
+
+import pdb
+
+
+# In[7]:
+
+
+x = [1,2,3]
+y=3
+z=4
+
+result = y+z
+pdb.set_trace()
+result1 = x+y
+
+
+# In[ ]:
+
+
+
+
